43292526b7c0c98f449c7fb738c09af06f9aeee0
Gary Alexander <gary.alexander@ucs-solutions.co.za>
2015-11-28 09:17:58 +0200

    Added gitswatch.sh

--- gitswatch.sh
+++ gitswatch.sh
@@ -1,11 +1,34 @@
-#!/bin/zsh
+#!/bin/bash
 # Will do a repeated git status in the current dir
-# Colorful output via spectrum
-autoload spectrum && spectrum
+# Colorful output is coming
+MYINSTALLDIR="/home/gta/Documents/scripting/src-gta/gitdevscripts"
+. "$MYINSTALLDIR/lib/colorecho/dist/ColorEcho.bash"
 
-while true
-do
-  echo "GITSWATCH" | toilet -F gay -f pagga -t
-  git status --short --branch
-  sleep 60
-done
+CURRENTDIR=`pwd`
+TMPFILE="/tmp/gitswatch.$$"
+
+if [ -d .git ]; then
+  while true
+  do
+    STATUS=`git status --short --branch` 
+    DATE=`date +'%F %R %a'`
+    clear
+    echo "GITSWATCH" | toilet -F gay -f smblock -t 
+    echo
+    echo.BoldYellow "STATUS"
+    echo.LightGreen "$STATUS"
+    echo.BoldYellow "CURRENTDIR"
+    echo.LightGreen "$CURRENTDIR"
+    echo.BoldYellow "DATE"
+    echo.LightGreen "$DATE" 
+    sec=60
+    while [ $sec -ge 0 ]; do
+      echo -ne "COUNTDOWN TO REFRESH: $sec SECS\033[0K\r" 
+      let "sec=sec-1"
+      sleep 1
+    done
+  done
+else
+  echo "Not a git repo!"
+  exit 1
+fi
