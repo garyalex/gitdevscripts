28a67247b98013cc2fb451a91b8592ce285c9d40
Gary Alexander <gary.alexander@ucs-solutions.co.za>
2015-11-29 13:00:24 +0200

    removed old coloring

--- gitswatch.sh
+++ gitswatch.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # Will do a repeated git status in the current dir
-# Colorful output is coming
-MYINSTALLDIR="/home/gta/Documents/scripting/src-gta/gitdevscripts"
-. "$MYINSTALLDIR/lib/colorecho/dist/ColorEcho.bash"
+# Gary Alexander
+# v0.0.2 - Colorful output is coming
 
 CURRENTDIR=`pwd`
-TMPFILE="/tmp/gitswatch.$$"
 
 if [ -d .git ]; then
   while true
@@ -15,18 +13,22 @@ if [ -d .git ]; then
     clear
     echo "GITSWATCH" | toilet -F metal -f smblock -t 
     echo
-    echo.BoldYellow "STATUS"
-    echo.LightGreen "$STATUS"
-    echo.BoldYellow "CURRENTDIR"
-    echo.LightGreen "$CURRENTDIR"
-    echo.BoldYellow "DATE"
-    echo.LightGreen "$DATE" 
+    echo "STATUS"
+    echo "$STATUS"
+    echo "CURRENTDIR"
+    echo "$CURRENTDIR"
+    echo "DATE"
+    echo "$DATE" 
+    if [[ $1 == "single" ]]; then
+      exit 0
+    else
     sec=60
-    while [ $sec -ge 0 ]; do
-      echo -ne "COUNTDOWN TO REFRESH: $sec SECS\033[0K\r" 
-      let "sec=sec-1"
-      sleep 1
-    done
+      while [ $sec -ge 0 ]; do
+        echo -ne "COUNTDOWN TO REFRESH: $sec SECS\033[0K\r" 
+        let "sec=sec-1"
+        sleep 1
+      done
+    fi
   done
 else
   echo "Not a git repo!"
